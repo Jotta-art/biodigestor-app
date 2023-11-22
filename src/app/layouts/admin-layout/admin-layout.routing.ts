8ba75e2e00412617616b1385037b7cc84d847386
Jotta-Art <joao.pedro42@outlook.com.br>
2023-11-22 15:55:53 -0300

    initial commit

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,19 @@
+import {Routes} from '@angular/router';
+
+import {DashboardComponent} from '../../pages/dashboard/dashboard.component';
+import {IconsComponent} from '../../pages/icons/icons.component';
+import {MapsComponent} from '../../pages/maps/maps.component';
+import {UserProfileComponent} from '../../pages/user-profile/user-profile.component';
+import {TablesComponent} from '../../pages/tables/tables.component';
+import {DispositivosComponent} from "../../pages/dispositivos/dispositivos.component";
+import {GraficosComponent} from "../../pages/graficos/graficos.component";
+
+export const AdminLayoutRoutes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'user-profile', component: UserProfileComponent},
+  {path: 'tables', component: TablesComponent},
+  {path: 'icons', component: IconsComponent},
+  {path: 'maps', component: MapsComponent},
+  {path: 'dispositivos', component: DispositivosComponent},
+  {path: 'graficos', component: GraficosComponent},
+];
---
+++ src/app/pages/graficos/graficos.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "../../variables/charts";
+
+@Component({
+  selector: 'app-dispositivos',
+  templateUrl: './graficos.component.html',
+  styleUrls: ['./graficos.component.scss']
+})
+export class GraficosComponent implements OnInit {
+  public datasets: any;
+  public data: any;
+  public clicked: boolean = true;
+  public clicked1: boolean = false;
+  public salesChart;
+
+  ngOnInit() {
+    var chartOrders = document.getElementById('chart-orders');
+
+    parseOptions(Chart, chartOptions());
+
+
+    var ordersChart = new Chart(chartOrders, {
+      type: 'bar',
+      options: chartExample2.options,
+      data: chartExample2.data
+    });
+
+    var chartSales = document.getElementById('chart-sales');
+
+    this.salesChart = new Chart(chartSales, {
+      type: 'line',
+      options: chartExample1.options,
+      data: chartExample1.data
+    });
+  }
+
+  public updateOptions() {
+    this.salesChart.data.datasets[0].data = this.data;
+    this.salesChart.update();
+  }
+
+}
