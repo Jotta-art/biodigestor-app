71f9172aed6631cb6db05ccaf3446b358a92e4ed
Jotta-Art <joao.pedro42@outlook.com.br>
2023-11-25 13:10:21 -0300

    adicionando primeng

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { ClipboardModule } from 'ngx-clipboard';
+import {ClipboardModule} from 'ngx-clipboard';
 
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AdminLayoutRoutes} from './admin-layout.routing';
+import {DashboardComponent} from '../../pages/dashboard/dashboard.component';
+import {IconsComponent} from '../../pages/icons/icons.component';
+import {MapsComponent} from '../../pages/maps/maps.component';
+import {UserProfileComponent} from '../../pages/user-profile/user-profile.component';
+import {TablesComponent} from '../../pages/tables/tables.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {DispositivosComponent} from "../../pages/dispositivos/dispositivos.component";
 import {GraficosComponent} from "../../pages/graficos/graficos.component";
+import {TableModule} from "primeng/table";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {DialogModule} from "primeng/dialog";
+import {InputTextModule} from "primeng/inputtext";
+import {CardModule} from "primeng/card";
+
 // import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -23,7 +31,13 @@ import {GraficosComponent} from "../../pages/graficos/graficos.component";
     FormsModule,
     HttpClientModule,
     NgbModule,
-    ClipboardModule
+    ClipboardModule,
+    TableModule,
+    ButtonModule,
+    RippleModule,
+    DialogModule,
+    InputTextModule,
+    CardModule
   ],
   declarations: [
     DashboardComponent,
