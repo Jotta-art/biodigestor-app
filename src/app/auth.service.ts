04e19e02e81ee2cdf53e51fd6a0984c37aee5bdb
Jotta-Art <joao.pedro42@outlook.com.br>
2023-11-26 16:31:37 -0300

    versao final, essa vai funcionar

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,7 +10,7 @@ import * as dotenv from 'dotenv';
   providedIn: 'root'
 })
 export class AuthService {
-  tokenURL = 'https://tcc-api-v1-fa6828c60737.herokuapp.com/oauth/token';
+  tokenURL = environment.baseUrl + '/oauth/token';
   clientId = 'my-angular-app';
   clientSecret = '@321';
 
