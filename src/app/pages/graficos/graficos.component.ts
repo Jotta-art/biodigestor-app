f7fa3128a94a3c5ae0a78b22ba709be2f32a1f86
Jotta-Art <joao.pedro42@outlook.com.br>
2023-11-27 15:33:17 -0300

    versao final?

--- src/app/pages/graficos/graficos.component.ts
+++ src/app/pages/graficos/graficos.component.ts
@@ -23,6 +23,7 @@ class TableData {
   vazaoEntrada;
   fluxoSaida;
   vazaoSaida;
+  pressao;
 }
 
 interface Grafico {
@@ -58,6 +59,7 @@ export class GraficosComponent implements OnInit {
   graficos: Grafico[];
   selectedGrafico: Grafico;
 
+
   ngOnInit() {
 
     this.app = initializeApp(this.firebaseConfig);
@@ -68,7 +70,8 @@ export class GraficosComponent implements OnInit {
 
     this.graficos = [
       {name: 'Real-Time', code: 'R'},
-      {name: 'Hour', code: ' H'},
+      {name: 'Consumo Entrada', code: ' E'},
+      {name: 'Consumo Saída', code: ' S'},
     ];
 
     this.selectedGrafico = {name: 'Real-Time', code: 'R'};
@@ -87,9 +90,14 @@ export class GraficosComponent implements OnInit {
         this.fetchDataFromFirebase();
         break;
 
-      case 'Hour':
-        this.initChartHour();
-        this.toggleChartType()
+      case 'Consumo Entrada':
+        this.initChartEntrada();
+        this.toggleChartTypeEntrada()
+        break;
+
+      case 'Consumo Saída':
+        this.initChartSaida();
+        this.toggleChartTypeSaida()
         break;
     }
   }
@@ -149,22 +157,26 @@ export class GraficosComponent implements OnInit {
         if (this.tableData === undefined) this.tableData = [];
 
         this.tableData.unshift({
-          id: agora.format('YYYY-MM-DD HH:mm:ss'),
-          fluxoEntrada: 'True',
-          vazaoEntrada: numeroAleatorio,
-          fluxoSaida: 'False',
-          vazaoSaida: numeroAleatorio2
+          id: agora.format('MM/DD/YYYY HH:mm:ss'),
+          fluxoEntrada: bioData[2],
+          vazaoEntrada: bioData[5],
+          fluxoSaida: bioData[3],
+          vazaoSaida: bioData[6],
+          pressao: bioData[4]
         });
 
         const myChart = this.chart;
 
+        // myChart.data.datasets[0].data.unshift(bioData[0]);//vazaoEntradaTotal
+        // myChart.data.datasets[1].data.unshift(bioData[1]);//vazaoSaidaTotal
+        // myChart.data.datasets[2].data.unshift(bioData[2]);//fluxoEntradaLigado
+        // myChart.data.datasets[3].data.unshift(bioData[3]);//fluxoSaidaLigado
+        // // myChart.data.datasets[3].data.unshift(bioData[4]);//pressao
+        // myChart.data.datasets[4].data.unshift(bioData[5]);//vazaoEntrada
+        // myChart.data.datasets[5].data.unshift(bioData[6]);//vazaoSaida
 
-        // myChart.data.datasets[0].data.unshift(bioData[0]);
-        console.log(Object.values(myChart.data.datasets[0].data))
-        // myChart.data.datasets[1].data.unshift(bioData[0]);
-
-        myChart.data.datasets[0].data.unshift(numeroAleatorio);
-        myChart.data.datasets[1].data.unshift(numeroAleatorio2);
+        myChart.data.datasets[0].data.unshift(bioData[5]);
+        myChart.data.datasets[1].data.unshift(bioData[6]);
 
         // Remova os dados mais antigos se a quantidade ultrapassar 100
         // Atualize o gráfico
@@ -176,7 +188,7 @@ export class GraficosComponent implements OnInit {
     }
   }
 
-  initChartHour() {
+  initChartEntrada() {
     if (this.chart) {
       this.chart.destroy(); // Destrua a instância do gráfico existente
     }
@@ -186,21 +198,21 @@ export class GraficosComponent implements OnInit {
     this.chart = new Chart(ctx, {
       type: 'bar', // Change type to 'bar' for a bar chart
       data: {
-        labels: Array.from({length: 7}, (_, i) => `${String(i).padStart(0, '1')} Hora`),
+        labels: Array.from({length: 11}, (_, i) => `${String(i).padStart(0, '10')} Min`),
         datasets: [{
-          label: 'Total',
+          label: 'Consumo Total Entrada',
           data: [], // Add your data for the first dataset
           backgroundColor: '#007bff',
           borderColor: '#007bff',
           borderWidth: 1
         }, {
-          label: 'Mínimo',
+          label: 'Entrada Mínima',
           data: [], // Add your data for the second dataset
           backgroundColor: '#fa5151',
           borderColor: '#fa5151',
           borderWidth: 1
         }, {
-          label: 'Máximo',
+          label: 'Entrada Máxima',
           data: [], // Add your data for the second dataset
           backgroundColor: '#87ea61',
           borderColor: '#87ea61',
@@ -224,16 +236,15 @@ export class GraficosComponent implements OnInit {
     });
   }
 
-  toggleChartType() {
-    for (let i = 0; i < 7; i++) {
-      let total = this.getRandomNumber(250, 500);
-      let minimo = this.getRandomNumber(1, 10);
-      let maximo = this.getRandomNumber(50, 100);
-      const agora = moment();
+  toggleChartTypeEntrada() {
+      let total = 270;
+      let minimo = 5;
+      let maximo = 75;
+      let agora = moment();
 
 
       this.tableDataHour.unshift({
-        id: agora.format('YYYY-MM-DD HH:mm:ss'),
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
         total: total,
         minimo: minimo,
         maximo: maximo
@@ -252,7 +263,583 @@ export class GraficosComponent implements OnInit {
 
       myChart.update();
 
+
+      total = 257;
+      minimo =  9;
+      maximo =  62;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 289;
+      minimo =  2;
+      maximo =  92;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 276;
+      minimo =  4;
+      maximo =  84;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 257;
+      minimo =  1;
+      maximo =  88;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 264;
+      minimo =  7;
+      maximo =  89;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 298;
+      minimo =  9;
+      maximo =  99;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 257;
+      minimo =  3;
+      maximo =  77;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 257;
+      minimo =  5;
+      maximo =  92;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 272;
+      minimo =  6;
+      maximo =  81;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+
+      total = 268;
+      minimo =  10;
+      maximo =  60;
+      agora = moment();
+
+
+      this.tableDataHour.unshift({
+        id: agora.format('MM/DD/YYYY HH:mm:ss'),
+        total: total,
+        minimo: minimo,
+        maximo: maximo
+
+      });
+
+
+      // myChart.data.datasets[0].data.unshift(bioData[0]);
+      // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+      myChart.data.datasets[0].data.unshift(total);
+      myChart.data.datasets[1].data.unshift(minimo);
+      myChart.data.datasets[2].data.unshift(maximo);
+
+      myChart.update();
+
+  }
+
+  initChartSaida() {
+    if (this.chart) {
+      this.chart.destroy(); // Destrua a instância do gráfico existente
     }
+
+    const ctx = document.getElementById("myChart");
+
+    this.chart = new Chart(ctx, {
+      type: 'bar', // Change type to 'bar' for a bar chart
+      data: {
+        labels: Array.from({length: 11}, (_, i) => `${String(i).padStart(0, '10')} Min`),
+        datasets: [{
+          label: 'Consumo Total Saída',
+          data: [], // Add your data for the first dataset
+          backgroundColor: '#ee00ff',
+          borderColor: '#ee00ff',
+          borderWidth: 1
+        }, {
+          label: 'Saída Mínima',
+          data: [], // Add your data for the second dataset
+          backgroundColor: '#fade51',
+          borderColor: '#fade51',
+          borderWidth: 1
+        }, {
+          label: 'Saída Máxima',
+          data: [], // Add your data for the second dataset
+          backgroundColor: '#ea9a61',
+          borderColor: '#ea9a61',
+          borderWidth: 1
+        }
+
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        },
+        legend: {
+          display: true, // Set display to true if you want to show the legend
+        }
+      }
+    });
+  }
+
+  toggleChartTypeSaida() {
+    let total = 290;
+    let minimo = 8;
+    let maximo = 65;
+    let agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+    const myChart = this.chart;
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 287;
+    minimo =  2;
+    maximo =  62;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 269;
+    minimo =  8;
+    maximo =  62;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 286;
+    minimo =  7;
+    maximo =  54;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 287;
+    minimo =  5;
+    maximo =  68;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 284;
+    minimo =  9;
+    maximo =  69;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 278;
+    minimo =  3;
+    maximo =  69;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 277;
+    minimo =  7;
+    maximo =  57;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 287;
+    minimo =  9;
+    maximo =  52;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 252;
+    minimo =  9;
+    maximo =  61;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
+
+    total = 298;
+    minimo =  4;
+    maximo =  80;
+    agora = moment();
+
+
+    this.tableDataHour.unshift({
+      id: agora.format('MM/DD/YYYY HH:mm:ss'),
+      total: total,
+      minimo: minimo,
+      maximo: maximo
+
+    });
+
+
+    // myChart.data.datasets[0].data.unshift(bioData[0]);
+    // myChart.data.datasets[1].data.unshift(bioData[0]);
+
+    myChart.data.datasets[0].data.unshift(total);
+    myChart.data.datasets[1].data.unshift(minimo);
+    myChart.data.datasets[2].data.unshift(maximo);
+
+    myChart.update();
+
   }
 
   getRandomNumber(min, max) {
